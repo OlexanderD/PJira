2c4c7dffac23a837abada2e8e6bd9010c2237fd5
OlexanderDOl <solenogo1234@gmail.com>
2024-09-30 17:32:50 +0300

    Initial commit

---
+++ Pjira.Infrastructure/Data/PjiraDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pjira.Infrastructure.Data
+{
+    public sealed class PjiraDbContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public PjiraDbContext(DbContextOptions<PjiraDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+}
