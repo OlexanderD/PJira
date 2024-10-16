09feba7ff362278b0fb8fd54d677c6fd0020652d
OlexanderDOl <solenogo1234@gmail.com>
2024-10-16 15:20:20 +0300

    Added src folder

--- src/Pjira.Infrastructure/DependencyInjection.cs
+++ src/Pjira.Infrastructure/DependencyInjection.cs
@@ -3,11 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pjira.Application.Common.Interfaces;
 using Pjira.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pjira.Infrastructure
 {
